c72b8caa21a7fed57d9cc30c622e304c19ffd282
unknown <ametovamir2013@gmail.com>
2024-05-04 19:09:48 +0500

    class work

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # CLASS WORK
-tst = 5
-if tst < 5:
-    print(False)
-else:
+tst = 15
+if tst > 10 or tst < 20:
     print(True)
+else:
+    print(False)
+
+
+# 2 work
+tst = -5
+if tst < -10 or tst > -8:
+    print(True)
+else:
+    print(False)
